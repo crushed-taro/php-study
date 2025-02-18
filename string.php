ce3fa9c969c97e2f3ae84d5d28ffd4ee5a9558fa
crushed-taro <97sinsang@gmail.com>
2025-02-18 14:45:48 +0900

    문자

---
+++ string.php
@@ -0,0 +1,8 @@
+<?php
+echo "Hello world" . "<br>";
+
+echo var_dump(1234) . "<br>";
+echo var_dump("1234") . "<br>";
+echo "hello" . "world" . "<br>";
+echo '그는 "안녕하세요 라고 말했다"' . "\n";
+echo "그는 \"안녕하세요 라고 말했다\"" . "\n";
